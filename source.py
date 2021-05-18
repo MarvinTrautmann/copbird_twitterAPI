6e41c1975f38b1b3404a9833a614a4efe4e6978d
MarvinTrautmann <m.trautmann@mailbox.org>
2021-05-18 21:35:54 +0200

    Create source.py

---
+++ source.py
@@ -0,0 +1,6 @@
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+tweet = api.get_status(id_of_tweet)
+print(tweet.text)
